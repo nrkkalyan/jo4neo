9b733bc92629cfe4da37d43db78b5e109007d25c
thewebsemantic <thewebsemantic@69c1ab5c-d873-11de-849f-b362a63f070a>
2010-01-09 17:22:57 +0000

    git-svn-id: https://jo4neo.googlecode.com/svn@405 69c1ab5c-d873-11de-849f-b362a63f070a

--- trunk/patterns/time/src/main/java/example/Main.java
+++ trunk/patterns/time/src/main/java/example/Main.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.Date;
 
 import jo4neo.ObjectGraph;
+import jo4neo.ObjectGraphFactory;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
@@ -29,7 +30,7 @@ public class Main {
 	public static void main(String[] args) throws MalformedURLException, IOException, ParseException {
 		
 		NeoService neo = new EmbeddedNeo("neo_store");
-		ObjectGraph graph = new ObjectGraph(neo);
+		ObjectGraph graph = ObjectGraphFactory.instance().get(neo);
 		
 		populateGraph(graph);
 		Transaction t = graph.beginTx();
